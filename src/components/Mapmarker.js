538c5b1ee73ad2bbc6211cef1455a165131cf815
cvonwilczur <cvonwilczur@gmail.com>
2018-07-19 15:34:49 -0700

    added styling to markers for visibility

--- src/components/Mapmarker.js
+++ src/components/Mapmarker.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import './MapMarkers.css';
 
-const Mapmarker = (props) => <div>{props.text}</div>;
+
+const Mapmarker = (props) => <div className="mapmarker">{props.text}</div>;
 
 export default Mapmarker;
